6a4dc71c757aaea2525bb8edd7e8492847e1b2dc
j7zzzzz <56579059+j7zzzzz@users.noreply.github.com>
2025-01-28 02:34:30 +0800

    Update power_calculation.R

--- Desktop/code/power_calculation.R
+++ Desktop/code/power_calculation.R
@@ -1,13 +1,12 @@
 library(survival)
-source('E:/Dropbox/Adaptive design/Covariate_adjusted/code/simulation/all_functions.R')
-
+source('Desktop/code/all_functions.R')
 
 
 ########################
 ### Main Program     ###
 ########################
 data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=case){
-  if(case=="caseR01"){ # z~multinomial(0.5,0.3,0.2)
+  if(case=="caseR01"){ # z~multinomial(0.26,0.48,0.26)
     z <- rmultinom(n,1,prb)
     minimization_z <- t(z)
     n_strata <- dim(z)[1]
@@ -17,12 +16,10 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     lambda0 <- lambda0
     cumuhazard <- rexp(n)
     HR <- exp(I*theta + colSums(beta_c * z[2:3,]))
-    #HR <- exp( I*theta + colSums(beta_c*z[1:length(beta_c),]) )
     t.star <- cumuhazard/lambda0/HR
     C <- runif(n,0,tau)
     t <- pmin(t.star, C)
     delta <- 1*(t.star<=C)
-    #data.simu<-data.frame(t, C, delta, I, 1-I, z[1,], z[2,], strata_z, minimization_z)[1:n,]
     data.simu<-data.frame(t, C, delta, I, 1-I, z[2,], z[3,], strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "C", "delta", "I1", "I0", "model_Z1","model_Z2",
                          paste0("strata",1:n_strata), paste0("minimization",1:n_strata))
@@ -30,7 +27,7 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
   } #Real case:RE01
   else if (case=="case3_R01"){ # coutinuous z #incorrect model
     n_category<-3
-    tmp<-discretize_z(n,0,1,n_category) #調整all_function p_cut選caseR01_3
+    tmp<-discretize_z(n,0,1,n_category) # To implement this case, kindly modify the p_cut in the discretize_z function within all_function to match the settings specified in case3_R01.
     z2_continous<-tmp$Z
     z2<-t(tmp$Z_dis)
     
@@ -41,18 +38,18 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     strata_z<-matrix(nrow=n,ncol = n_strata)
     strata_z <- t(z2)
     I<-treatment_assignment(n,strata_z,minimization_z,randomization,p_trt)
-    lambda0<-lambda0 #log(2)/12
+    lambda0<-lambda0
     cumuhazard<-rexp(n)
     HR<-exp( I*theta + beta_c[1]*z2_continous )
     t.star<-cumuhazard/lambda0/HR
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I,z2_continous,strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z2_continous, strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0","model_z2",paste0("strata",1:n_strata),paste("minimization",1:minimization_n_col))
     return(data.simu)
   }
-  else if (case=="case4_R01"){ # z1~multinomial(0.5,0.3,0.2)+z2~N(0,1) K=3 # z1~multinomial(0.4,0.3,0.2,0.1)+z2~N(0,1) K=3
+  else if (case=="case4_R01"){ # z1~multinomial(0.26,0.48,0.26)+z2~N(0,1) with K=3
     z1<-rmultinom(n,1,prb) 
     row.names(z1)<-paste0("z1_",1:nrow(z1))
     n_category<-3
@@ -73,14 +70,14 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
       }
     }
     I<-treatment_assignment(n,strata_z,minimization_z,randomization,p_trt)
-    lambda0<-lambda0 #log(2)/12
+    lambda0<-lambda0
     cumuhazard<-rexp(n)
     HR<-exp( I*theta + colSums(beta_c * z1[2:3,]) + 1*z2_continous^2)
     t.star<-cumuhazard/lambda0/HR
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I,z1[1,],z1[2,],z2_continous,strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z1[1,], z1[2,], z2_continous, strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0",paste0("model_z1",1:2),"model_z2",paste0("strata",1:n_strata),paste("minimization",1:minimization_n_col))
     return(data.simu)
   }
@@ -97,7 +94,7 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I, z[1,], z[2,],strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z[1,], z[2,], strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0","model_Z1","model_Z2",paste0("strata",1:n_strata),paste0("minimization",1:minimization_n_col))
     return(data.simu)
   }
@@ -111,12 +108,10 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     lambda0 <- lambda0
     cumuhazard <- rexp(n)
     HR <- exp(I*theta + colSums(beta_c * z[2:4,]))
-    #HR <- exp( I*theta + colSums(beta_c*z[1:length(beta_c),]) )
     t.star <- cumuhazard/lambda0/HR
     C <- runif(n,0,tau)
     t <- pmin(t.star, C)
     delta <- 1*(t.star<=C)
-    #data.simu<-data.frame(t, C, delta, I, 1-I, z[1,], z[2,], strata_z, minimization_z)[1:n,]
     data.simu<-data.frame(t, C, delta, I, 1-I, z[2,], z[3,], z[4,], strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "C", "delta", "I1", "I0", "model_Z1","model_Z2","model_Z3",
                          paste0("strata",1:n_strata), paste0("minimization",1:n_strata))
@@ -124,7 +119,7 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
   }   #Real case:Lung cancer
   else if (case=="case3_L"){ # coutinuous z #incorrect model
     n_category<-4
-    tmp<-discretize_z(n,0,1,n_category) #調整all_function p_cut選caseL_3
+    tmp<-discretize_z(n,0,1,n_category) # To implement this case, kindly modify the p_cut in the discretize_z function within all_function to match the settings specified in case3_L.
     z2_continous<-tmp$Z
     z2<-t(tmp$Z_dis)
     
@@ -135,18 +130,18 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     strata_z<-matrix(nrow=n,ncol = n_strata)
     strata_z <- t(z2)
     I<-treatment_assignment(n,strata_z,minimization_z,randomization,p_trt)
-    lambda0<-lambda0 #log(2)/12
+    lambda0<-lambda0
     cumuhazard<-rexp(n)
     HR<-exp( I*theta + beta_c[1]*z2_continous )
     t.star<-cumuhazard/lambda0/HR
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I,z2_continous,strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z2_continous, strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0","model_z2",paste0("strata",1:n_strata),paste("minimization",1:minimization_n_col))
     return(data.simu)
   }
-  else if (case=="case4_L"){ # z1~multinomial(0.5,0.3,0.2)+z2~N(0,1) K=3 # z1~multinomial(0.4,0.3,0.2,0.1)+z2~N(0,1) K=3
+  else if (case=="case4_L"){ # z1~multinomial(0.25,0.35,0.2,0.2)+z2~N(0,1) with K=3
     z1<-rmultinom(n,1,prb) 
     row.names(z1)<-paste0("z1_",1:nrow(z1))
     n_category<-3
@@ -167,14 +162,14 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
       }
     }
     I<-treatment_assignment(n,strata_z,minimization_z,randomization,p_trt)
-    lambda0<-lambda0 #log(2)/12
+    lambda0<-lambda0
     cumuhazard<-rexp(n)
     HR<-exp( I*theta + colSums(beta_c * z1[2:4,]) + 1*z2_continous^2)
     t.star<-cumuhazard/lambda0/HR
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I,z1[1,],z1[2,],z1[3,],z2_continous,strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z1[1,], z1[2,], z1[3,], z2_continous,strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0",paste0("model_z1",1:3),"model_z2",paste0("strata",1:n_strata),paste("minimization",1:minimization_n_col))
     return(data.simu)
   }
@@ -191,7 +186,7 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I, z[1,], z[2,], z[3,],strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z[1,], z[2,], z[3,], strata_z,minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0","model_Z1","model_Z2","model_Z3",paste0("strata",1:n_strata),paste0("minimization",1:minimization_n_col))
     return(data.simu)
   }
@@ -204,7 +199,6 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     I <- treatment_assignment(n, strata_z, minimization_z, randomization, p_trt)
     lambda0 <- lambda0
     cumuhazard <- rexp(n)
-    #HR <- exp(I*theta+1*z[1,]+1*z[2,])
     HR <- exp( I*theta + colSums(beta_c*z[1:length(beta_c),]) )
     t.star <- cumuhazard/lambda0/HR
     C <- runif(n,0,tau)
@@ -224,7 +218,6 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     I <- treatment_assignment(n, strata_z, minimization_z, randomization, p_trt)
     lambda0 <- lambda0
     cumuhazard <- rexp(n)
-    #HR <- exp(I*theta+1*z[1,]+1*z[2,])
     HR <- exp( I*theta + colSums(beta_c*z[1:length(beta_c),]) )
     t.star <- cumuhazard/lambda0/HR
     C <- runif(n,0,tau)
@@ -237,7 +230,7 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
   }
   else if (case=="case3_K=3"){ # coutinuous z #incorrect model
     n_category<-3
-    tmp<-discretize_z(n,0,1,n_category) #all_funtion的p_cut要選case3_K=3
+    tmp<-discretize_z(n,0,1,n_category) # To implement this case, kindly modify the p_cut in the discretize_z function within all_function to match the settings specified in case3_K=3.
     z2_continous<-tmp$Z
     z2<-t(tmp$Z_dis)
     
@@ -248,20 +241,20 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     strata_z<-matrix(nrow=n,ncol = n_strata)
     strata_z <- t(z2)
     I<-treatment_assignment(n,strata_z,minimization_z,randomization,p_trt)
-    lambda0<-lambda0 #log(2)/12
+    lambda0<-lambda0
     cumuhazard<-rexp(n)
     HR<-exp( I*theta + beta_c[1]*z2_continous )
     t.star<-cumuhazard/lambda0/HR
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I,z2_continous,strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z2_continous, strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0","model_z2",paste0("strata",1:n_strata),paste("minimization",1:minimization_n_col))
     return(data.simu)
   }
   else if (case=="case3_K=4"){ # coutinuous z #incorrect model
     n_category<-4
-    tmp<-discretize_z(n,0,1,n_category) #all_funtion的p_cut要選case3_K=4
+    tmp<-discretize_z(n,0,1,n_category) # To implement this case, kindly modify the p_cut in the discretize_z function within all_function to match the settings specified in case3_K=4.
     z2_continous<-tmp$Z
     z2<-t(tmp$Z_dis)
     
@@ -272,18 +265,18 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     strata_z<-matrix(nrow=n,ncol = n_strata)
     strata_z <- t(z2)
     I<-treatment_assignment(n,strata_z,minimization_z,randomization,p_trt)
-    lambda0<-lambda0 #log(2)/12
+    lambda0<-lambda0
     cumuhazard<-rexp(n)
     HR<-exp( I*theta + beta_c[1]*z2_continous )
     t.star<-cumuhazard/lambda0/HR
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I,z2_continous,strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z2_continous, strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0","model_z2",paste0("strata",1:n_strata),paste("minimization",1:minimization_n_col))
     return(data.simu)
   }
-  else if (case=="case4_K=3"){ # z1~multinomial(0.5,0.3,0.2)+z2~N(0,1) K=3 # z1~multinomial(0.4,0.3,0.2,0.1)+z2~N(0,1) K=3
+  else if (case=="case4_K=3"){ # z1~multinomial(0.5,0.3,0.2)+z2~N(0,1) with K=3
     z1<-rmultinom(n,1,prb) 
     row.names(z1)<-paste0("z1_",1:nrow(z1))
     n_category<-3
@@ -304,18 +297,18 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
       }
     }
     I<-treatment_assignment(n,strata_z,minimization_z,randomization,p_trt)
-    lambda0<-lambda0 #log(2)/12
+    lambda0<-lambda0
     cumuhazard<-rexp(n)
     HR<-exp( I*theta + colSums(beta_c*z1[1:length(beta_c),]) + 1*z2_continous^2)
     t.star<-cumuhazard/lambda0/HR
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I,z1[1,],z1[2,],z2_continous,strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z1[1,], z1[2,], z2_continous, strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0",paste0("model_z1",1:2),"model_z2",paste0("strata",1:n_strata),paste("minimization",1:minimization_n_col))
     return(data.simu)
   }
-  else if (case=="case4_K=4"){ # z1~multinomial(0.5,0.3,0.2)+z2~N(0,1) K=3 # z1~multinomial(0.4,0.3,0.2,0.1)+z2~N(0,1) K=3
+  else if (case=="case4_K=4"){ # z1~multinomial(0.4,0.3,0.2,0.1)+z2~N(0,1) with K=3
     z1<-rmultinom(n,1,prb) 
     row.names(z1)<-paste0("z1_",1:nrow(z1))
     n_category<-3
@@ -336,14 +329,14 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
       }
     }
     I<-treatment_assignment(n,strata_z,minimization_z,randomization,p_trt)
-    lambda0<-lambda0 #log(2)/12
+    lambda0<-lambda0
     cumuhazard<-rexp(n)
     HR<-exp( I*theta + colSums(beta_c*z1[1:length(beta_c),]) + 1*z2_continous^2)
     t.star<-cumuhazard/lambda0/HR
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I,z1[1,],z1[2,],z1[3,],z2_continous,strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z1[1,], z1[2,], z1[3,], z2_continous, strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0",paste0("model_z1",1:3),"model_z2",paste0("strata",1:n_strata),paste("minimization",1:minimization_n_col))
     return(data.simu)
   }
@@ -360,7 +353,7 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I, z[1,], z[2,],strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z[1,], z[2,], strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0","model_Z1","model_Z2",paste0("strata",1:n_strata),paste0("minimization",1:minimization_n_col))
     return(data.simu)
   }
@@ -377,7 +370,7 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
     C<-runif(n,0,tau)
     t<-pmin(t.star,C)
     delta<-1*(t.star<=C)
-    data.simu<-data.frame(t, delta, I, 1-I, z[1,], z[2,], z[3,],strata_z,minimization_z)[1:n,]
+    data.simu<-data.frame(t, delta, I, 1-I, z[1,], z[2,], z[3,], strata_z, minimization_z)[1:n,]
     names(data.simu)<- c("t", "delta", "I1", "I0","model_Z1","model_Z2","model_Z3",paste0("strata",1:n_strata),paste0("minimization",1:minimization_n_col))
     return(data.simu)
   }
@@ -388,42 +381,41 @@ data_gen <- function(n,theta,randomization,p_trt,lambda0,prb,beta_c,tau,case=cas
 ##############################################################################
 #------------------------------------Example----------------------------------
 # n = 428
-# theta = 0.0
+# theta = 0.3
 # Pow = 0.8
-# randomization=c("SR") #"SR","CABC","permuted_block","minimization","urn"
+# randomization=c("permuted_block") #"SR","CABC","permuted_block"
 # 
-# lambda0 = 0.05 
+# lambda0 = 0.05
 # tau_a = 25; tau = 30  # C~U(0,tau); W~U(0, tau_a)
-# p_trt <- 1/2 
-# 
-# 
-# # prb <- c(0.4, 0.3, 0.2, 0.1)  # multinomial distribution
-# # beta_c <- c(1, 1, 1)
-# 
+# p_trt <- 1/2
+# #
+# #
+# # #-- 4 groups
+# # # prb <- c(0.4, 0.3, 0.2, 0.1)  # multinomial distribution
+# # # beta_c <- c(1, 1, 1)
+# # #-- 3 groups
 # prb <- c(0.5, 0.3, 0.2)  # multinomial distribution
 # beta_c <- c(1, 1)
+# #
+# #
+# alpha <- 0.05
 # 
-# 
-# alpha = 0.05
-# 
-# case <- "case1"
-# simu = 1000
+# case <- "case1" # Simulation:"case1","case2","case3_K=3","case3_K=4","case4_K=3","case4_K=4","case5_K=3","case5_K=4"
+#                   # Real case-RE01:"caseR01,"case3_R01","case4_R01","case5_R01"
+#                   # Real case-Lung cancer:"caseL,"case3_L","case4_L","case5_L"
+# simu <- 1000
 
 ###############################################################################
 #####################################        Main Program         #############
 ###############################################################################
-SIMU_all <- function(n, theta, Pow, randomization, lambda0, tau_a, tau, p_trt, prb, beta_c, case, simu){
-
-alpha = 0.05
-
-p_Wd <- p_LR <- p_StrLR <- p_stdLR <- p_RoLR <- p_TS <- p_RS <- p_TLR <- p_stdLR1 <- k <- rep(NA, simu)
-C_alpha <- exp(-qchisq(alpha, df=4, lower.tail = F)/2)
-  
-for (i in 1:simu){
+SIMU <- function(n, theta, Pow, randomization, lambda0, tau_a, tau, p_trt, alpha, prb, beta_c, case, simu){
+  p_Wd <- p_LR <- p_StrLR <- p_stdLR <- p_RoLR <- p_TS <- p_RS <- p_TLR <- p_stdLR1 <- k <- rep(NA, simu)
+  C_alpha <- exp(-qchisq(alpha, df = 4, lower.tail = F)/2)
+  for (i in 1:simu){
     
-  data.simu <- Calender_date <- D_all <- NA
-  data.simu <- data_gen(n, theta=theta, randomization=randomization, beta_c=beta_c,
-                          lambda0=lambda0, prb=prb, tau=tau, p_trt=p_trt, case=case)
+    data.simu <- Calender_date <- D_all <- NA
+    data.simu <- data_gen(n, theta=theta, randomization=randomization, beta_c=beta_c,
+                        lambda0=lambda0, prb=prb, tau=tau, p_trt=p_trt, case=case)
     
     
     ### ----------------- original data include  -------------
@@ -466,7 +458,7 @@ for (i in 1:simu){
     ### proposed T_RS in Ye and Shao, 2020, jrssb. ###
     score.test_robust <- score_robust (D_all,randomization,p_trt)
     p_RS[i] <- unlist(score_robust (D_all,randomization,p_trt))[1] > qnorm(alpha, lower.tail = F)
-  }
+    }
   
   
   Tab1 = data.frame(death = round(mean(k,na.rm = TRUE), 0),
@@ -480,7 +472,7 @@ for (i in 1:simu){
   )
   
   print(paste("death.rate=", round(round(mean(k,na.rm = TRUE), 0)/n, 4) , "|death = ", round(mean(k,na.rm = TRUE), 0)))
-  print(paste("| n=",n , "| theta=", theta, "| Power=", Pow, "| Design=", randomization))
+  print(paste("n=",n , "| theta=", theta, "| Power=", Pow, "| Design=", randomization))
   return(Tab1)
 
 }
@@ -489,7 +481,4 @@ for (i in 1:simu){
 #################        Function         ##############
 ########################################################
 SIMU(n=428, theta=0.3, Pow=0.8, randomization=c("permuted_block"), lambda0=0.05, tau_a=25, tau=30, 
-     p_trt=1/2, prb=c(0.5, 0.3, 0.2), beta_c=c(1, 1), case="case1", simu=1000)
-
-
-
+     p_trt=1/2, alpha=0.05, prb=c(0.5, 0.3, 0.2), beta_c=c(1, 1), case="case1", simu=1000)
